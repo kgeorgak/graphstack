64e97739e7940b557c5ee8468291e9bf8dbde5c2
Kostas Georgakopoulos <kostas.georgakopoulos@rackspace.co.uk>
2015-01-08 13:04:21 +0000

    Initial Commit

---
+++ Berksfile
@@ -0,0 +1,5 @@
+source "https://supermarket.getchef.com"
+
+cookbook 'statsd', git:'https://github.com/hectcastro/chef-statsd'
+
+metadata
---
+++ metadata.rb
@@ -0,0 +1,7 @@
+name             'graphstack'
+maintainer       'YOUR_NAME'
+maintainer_email 'YOUR_EMAIL'
+license          'All rights reserved'
+description      'Installs/Configures graphstack'
+long_description 'Installs/Configures graphstack'
+version          '0.1.0'
