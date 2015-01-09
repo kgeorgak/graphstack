ba7a7444d762cc1e598102d3f5794da2847bf300
Kostas Georgakopoulos <kostas.georgakopoulos@rackspace.co.uk>
2015-01-09 07:39:15 +0000

    testing

--- metadata.rb
+++ metadata.rb
@@ -5,3 +5,5 @@ license          'All rights reserved'
 description      'Installs/Configures graphstack'
 long_description 'Installs/Configures graphstack'
 version          '0.1.0'
+
+depends 'statsd'
--- recipes/default.rb
+++ recipes/default.rb
@@ -6,3 +6,7 @@
 #
 # All rights reserved - Do Not Redistribute
 #
+
+node.default['statsd']['nodejs_bin'] = '/usr/bin/node'
+
+include_recipe 'statsd'
